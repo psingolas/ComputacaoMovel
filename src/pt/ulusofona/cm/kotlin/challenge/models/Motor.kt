d56118e00b75feea23193ddee56b6c75b7559b4e
sacra <sacramentojoel@gmail.com>
2022-02-28 18:01:54 +0000

    v0

--- src/pt/ulusofona/cm/kotlin/challenge/models/Motor.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/models/Motor.kt
@@ -1,5 +1,5 @@
 package pt.ulusofona.cm.kotlin.challenge.models
 
-class Motor (var cavalos:Int,cilindrada: Int,var ligado:Boolean){
+class Motor (var cavalos:Int,cilindrada: Int,var ligado:Boolean=false){
 
 }
--- src/pt/ulusofona/cm/kotlin/challenge/models/Veiculo.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/models/Veiculo.kt
@@ -3,5 +3,7 @@ package pt.ulusofona.cm.kotlin.challenge.models
 import pt.ulusofona.cm.kotlin.challenge.models.Posicao
 import java.util.*
 
-class Veiculo(var identificador:String, var posicao: Posicao, var dataDeAquisicao: Date) {
+abstract class Veiculo(var identificador:String, var posicao: Posicao= Posicao(0,0), var dataDeAquisicao: Date=Date()) {
+    abstract fun requerCarta(): Boolean
+
 }
