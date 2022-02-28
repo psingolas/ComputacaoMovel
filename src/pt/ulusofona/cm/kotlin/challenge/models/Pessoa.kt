dc62f37a2e608d12af4b6052b0ea53eda87bfba5
sacra <sacramentojoel@gmail.com>
2022-02-28 17:40:42 +0000

    v0

--- src/pt/ulusofona/cm/kotlin/challenge/models/Pessoa.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/models/Pessoa.kt
@@ -1,9 +1,19 @@
 package pt.ulusofona.cm.kotlin.challenge.models
 
+import java.text.SimpleDateFormat
 import java.util.*
 
-abstract class Pessoa (var nome:String, var veiculo: MutableList<Veiculo> = mutableListOf<Veiculo>(), var dataDeNascimento:Date, var posicao: Posicao) {
+abstract class Pessoa (var nome:String, var veiculo: MutableList<Veiculo> = mutableListOf<Veiculo>(), var dataDeNascimento:Date, var carta: Carta?,var posicao: Posicao) {
+fun temCarta(): Boolean{
+    if (carta== null){
+        return false
+    }else{
+        return true
+    }
+}
+    override fun toString(): String {
 
-
+        return "Pessoa | $nome | $dataDeNascimento | Posicao | x:${this.posicao.x} | y:${this.posicao.y}"
+    }
 
 }
