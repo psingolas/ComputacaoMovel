26873a81e4e181bf86b799ef891d9d5b22ac55e3
sacra <sacramentojoel@gmail.com>
2022-03-05 21:35:28 +0000

    v0

--- src/pt/ulusofona/cm/kotlin/challenge/models/Carro.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/models/Carro.kt
@@ -2,20 +2,20 @@ package pt.ulusofona.cm.kotlin.challenge.models
 
 class Carro(var identificador: String, var motor: Motor) {
 
+    fun ligar() {
+        motor.ligar()
+    }
+    fun desligar(){
+        motor.desligar()
+    }
+    fun requerCarta():Boolean{
+        return true
+    }
+    fun estaLigado(){
+        motor.estaLigado()
+    }
 
     override fun toString(): String {
-        fun ligar() {
-            motor.ligar()
-        }
-        fun desligar(){
-            motor.desligar()
-        }
-        fun requerCarta():Boolean{
-            return true
-        }
-        fun estaLigado(){
-            motor.estaLigado()
-        }
         return "Carro | $identificador | data de aquisicao | Posicao | x:0 | y:0"
     }
 
