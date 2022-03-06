f098a0a38c1125e2d0ee849ec1eec4d047aebfb3
sacra <sacramentojoel@gmail.com>
2022-03-06 15:19:58 +0000

    v0

--- src/pt/ulusofona/cm/kotlin/challenge/models/Bicicleta.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/models/Bicicleta.kt
@@ -3,24 +3,24 @@ package pt.ulusofona.cm.kotlin.challenge.models
 import pt.ulusofona.cm.kotlin.challenge.exceptions.AlterarPosicaoException
 import java.text.SimpleDateFormat
 
-open class Bicicleta(var identificador: String) {
-      lateinit var pos: Veiculo
+open class Bicicleta(override var identificador: String):Veiculo(identificador) {
+      //lateinit var pos:Veiculo
 
     //para haver um override é necessário que a outra class seja open
-    fun requerCarta(): Boolean {
+    override fun requerCarta(): Boolean {
         return false //para andar de bicicleta não é necessário carta
     }
 
 
      fun moverPara(x: Int, y: Int) {
         //pos.posicao.changePosition(x, y)
-        return pos.posicao.changePosition(x,y)//na class Veiculo tem a funcao para alterar a posicao
+        return posicao.changePosition(x,y)//na class Veiculo tem a funcao para alterar a posicao
     }
 
     override fun toString(): String {
         val dataForm=SimpleDateFormat()
-        val dataDeAquisicao=dataForm.format(pos.dataDeAquisicao)
-        return "Bicicleta | $identificador | $dataDeAquisicao| Posicao | x:${this.pos.posicao.x} | y:${this.pos.posicao.y}"
+        val dataDeAquisicao=dataForm.format(dataDeAquisicao)
+        return "Bicicleta | $identificador | $dataDeAquisicao| Posicao | x:${this.posicao.x} | y:${this.posicao.y}"
     }
 
 }
