323760cbd90a3d9a66456d130955248270d83bce
sacra <sacramentojoel@gmail.com>
2022-03-06 19:14:34 +0000

    v0

--- src/pt/ulusofona/cm/kotlin/challenge/models/Posicao.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/models/Posicao.kt
@@ -2,7 +2,7 @@ package pt.ulusofona.cm.kotlin.challenge.models
 
 import pt.ulusofona.cm.kotlin.challenge.exceptions.AlterarPosicaoException
 
-open class Posicao(var x: Int, var y: Int) {
+data class Posicao(var x: Int, var y: Int) {
 
     fun changePosition(novoPosX: Int, novoPosY: Int) {
 
