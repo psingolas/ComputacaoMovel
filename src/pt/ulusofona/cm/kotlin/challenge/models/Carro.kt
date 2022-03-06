fcc72c73c6e840042bdebdf414c76dcebfa688f8
sacra <sacramentojoel@gmail.com>
2022-03-06 19:28:50 +0000

    v0

--- src/pt/ulusofona/cm/kotlin/challenge/models/Carro.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/models/Carro.kt
@@ -3,24 +3,37 @@ package pt.ulusofona.cm.kotlin.challenge.models
 import pt.ulusofona.cm.kotlin.challenge.interfaces.Ligavel
 import java.text.SimpleDateFormat
 
-class Carro(override var identificador: String,  var motor: Motor):Veiculo(identificador),Ligavel {
+class Carro(override var identificador: String, var motor: Motor) : Veiculo(identificador), Ligavel {
+
+
+    override fun moverPara(x: Int, y: Int) {
+        if (motor.estaLigado() == false) {
+            motor.ligar() // o ligar() é da funcao presente do Motor
+        }
+        posicao.changePosition(x, y)
+        motor.desligar()
+    }
+
 
     override fun ligar() {
         motor.ligar()
     }
-      override fun desligar(){
+
+    override fun desligar() {
         motor.desligar()
     }
-     override fun requerCarta():Boolean{
+
+    override fun requerCarta(): Boolean {
         return true
     }
-      override fun estaLigado(): Boolean {
+
+    override fun estaLigado(): Boolean {
         return motor.estaLigado()
     }
 
     override fun toString(): String {
-        val dataForm= SimpleDateFormat("dd-MM-yyyy")
-        val dataDeAquisicao=dataForm.format(dataDeAquisicao)
+        val dataForm = SimpleDateFormat("dd-MM-yyyy")
+        val dataDeAquisicao = dataForm.format(dataDeAquisicao)
         return "Carro | $identificador | $dataDeAquisicao | Posicao | x:${this.posicao.x} | y:${this.posicao.y}"
     }
 
