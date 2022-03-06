6c9342462628f7d4659de23f12a72613f911fc1b
sacra <sacramentojoel@gmail.com>
2022-03-06 18:26:38 +0000

    v0

--- src/pt/ulusofona/cm/kotlin/challenge/interfaces/Ligavel.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/interfaces/Ligavel.kt
@@ -1,8 +1,12 @@
 package pt.ulusofona.cm.kotlin.challenge.interfaces
 
+import pt.ulusofona.cm.kotlin.challenge.exceptions.VeiculoLigadoException
+
 
 interface Ligavel {
+    @Throws(VeiculoLigadoException::class)
     fun ligar()
+
     fun desligar()
     fun estaLigado(): Boolean
 }
