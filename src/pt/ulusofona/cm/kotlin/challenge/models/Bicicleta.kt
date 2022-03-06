4848a2e4bb31e370d312a246b94225f027a76505
sacra <sacramentojoel@gmail.com>
2022-03-06 19:09:46 +0000

    v0

--- src/pt/ulusofona/cm/kotlin/challenge/models/Bicicleta.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/models/Bicicleta.kt
@@ -3,14 +3,14 @@ package pt.ulusofona.cm.kotlin.challenge.models
 import pt.ulusofona.cm.kotlin.challenge.exceptions.AlterarPosicaoException
 import java.text.SimpleDateFormat
 
- class Bicicleta(override var identificador: String):Veiculo(identificador) {
+ class Bicicleta( identificador: String):Veiculo(identificador) {
       //lateinit var pos:Veiculo
 
     //para haver um override é necessário que a outra class seja open
     override fun requerCarta(): Boolean {
         return false //para andar de bicicleta não é necessário carta
     }
-    @Throws(AlterarPosicaoException::class)
+
     override fun moverPara(x: Int, y: Int) {
         //pos.posicao.changePosition(x, y)
          posicao.changePosition(x,y)//na class Veiculo tem a funcao para alterar a posicao
