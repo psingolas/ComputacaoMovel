985388b99153b516042e9372eeff41fa357dd358
sacra <sacramentojoel@gmail.com>
2022-03-06 14:55:47 +0000

    v0

--- src/pt/ulusofona/cm/kotlin/challenge/models/Veiculo.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/models/Veiculo.kt
@@ -3,13 +3,6 @@ package pt.ulusofona.cm.kotlin.challenge.models
 import pt.ulusofona.cm.kotlin.challenge.models.Posicao
 import java.util.*
 
-abstract class Veiculo(open var identificador:String, var posicao: Posicao= Posicao(0,0), var dataDeAquisicao: Date=Date()) {
+  abstract class Veiculo(open var identificador:String, var posicao: Posicao= Posicao(0,0), var dataDeAquisicao: Date=Date()) {
     abstract fun requerCarta(): Boolean
-
-    //fun moverPara(): Posicao{
-
-     //}
-     open fun moverPara(x: Int, y: Int) {
-        //posicao.alterarPosicaoPara(x, y)
-    }
 }
