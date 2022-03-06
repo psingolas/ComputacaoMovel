45e06a8b429d325ad90b53fbf4752105564a1a72
sacra <sacramentojoel@gmail.com>
2022-03-06 20:09:52 +0000

    v0

--- src/pt/ulusofona/cm/kotlin/challenge/interfaces/Movimentavel.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/interfaces/Movimentavel.kt
@@ -1,5 +1,8 @@
 package pt.ulusofona.cm.kotlin.challenge.interfaces
 
+import pt.ulusofona.cm.kotlin.challenge.exceptions.AlterarPosicaoException
+
 interface Movimentavel{
+    @Throws(AlterarPosicaoException::class)
     fun moverPara(x:Int, y:Int)
 }
